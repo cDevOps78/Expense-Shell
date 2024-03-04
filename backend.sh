c9421c204841b92824ad4c9cc42d805c1c63955d
chaitu1 <chaitu143@gmail.com>
2024-03-04 12:21:14 +0530

    backend automation is ready

---
+++ backend.sh
@@ -0,0 +1,29 @@
+scriptPath=$(realpath "$PWD")
+
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+dnf install nodejs -y
+
+useradd expense
+
+mkdir /app 
+
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip 
+cd /app 
+unzip /tmp/backend.zip
+
+cd /app 
+npm install 
+
+cp $scriptPath/backend.service /etc/systemd/system/backend.service
+
+systemctl daemon-reload
+
+systemctl enable backend 
+systemctl start backend
+
+dnf install mysql -y
+
+mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pExpenseApp@1 < /app/schema/backend.sql
+
+
