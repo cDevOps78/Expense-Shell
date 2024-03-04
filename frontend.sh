24ebee1911a8941a79bcd3d3e6a4e13d5a7341fd
chaitu1 <chaitu143@gmail.com>
2024-03-04 11:47:03 +0530

    frontend.sh automation is ready

---
+++ frontend.sh
@@ -0,0 +1,16 @@
+dnf install nginx -y
+
+systemctl start nginx 
+
+rm -rf /usr/share/nginx/html/* 
+
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip 
+
+unzip /tmp/frontend.zip -d  /usr/share/nginx/html/
+
+cp expense.conf /etc/nginx/default.d/expense.conf 
+
+systemctl restart nginx
+
+
+systemctl enable nginx
