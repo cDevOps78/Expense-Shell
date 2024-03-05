3a22c12a19bc7a06cfe67080b62cf6b4479c0602
chaitu1 <chaitu143@gmail.com>
2024-03-05 20:44:03 +0530

    All cnages

--- backend.sh
+++ backend.sh
@@ -4,40 +4,40 @@ component="backend"
 source $scriptPath/common.sh
 
 printHead "disable nodejs"
-dnf module disable nodejs -y &>> /dev/null
+dnf module disable nodejs -y &> $logFileLocation
 checkStatus "disable nodejs"
 
 printHead "enable nodejs"
-dnf module enable nodejs:20 -y &>> /dev/null
+dnf module enable nodejs:20 -y &>> $logFileLocation 
 checkStatus "enable nodejs"
 
 printHead "Install nodejs"
-dnf install nodejs -y &>> /dev/null
+dnf install nodejs -y &>> $logFileLocation
 checkStatus "Install nodejs"
 
 printHead "Adding user"
-id expense &>> /dev/null
+id expense &>> $logFileLocation
 if [ $? -ne 0 ];then
 useradd expense 
 fi
 checkStatus "Adding user"
 
 printHead "Creating ddirectory"
-mkdir -p /app &>> /dev/null
+mkdir -p /app &>> $logFileLocation
 checkStatus "Creating ddirectory"
 
 printHead "Downloading content"
-curl  -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip  &>> /dev/null
+curl  -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip  &>> $logFileLocation
 checkStatus "Downloading content"
 
 printHead "Unzipping content"
-unzip -q -o /tmp/backend.zip -d /app &>> /dev/null
+unzip -q -o /tmp/backend.zip -d /app &>> $logFileLocation
 checkStatus "Unzipping content"
 
 cd /app 
 
 printHead "npm install"
-npm install &>> /dev/null
+npm install &>> $logFileLocation
 checkStatus "npm install"
 
 printHead "copying-script backend.service"
@@ -45,22 +45,22 @@ cp $scriptPath/backend.service /etc/systemd/system/backend.service
 checkStatus "copying-script backend.service"
 
 printHead "Daemon-reload"
-systemctl daemon-reload &>> /dev/null
+systemctl daemon-reload &>> $logFileLocation
 checkStatus "Daemon-reload"
 
 printHead "starting-backend"
-systemctl enable backend  &>> /dev/null
+systemctl enable backend  &>> $logFileLocation
 checkStatus "starting-backend"
 
 printHead "starting-backend"
-systemctl start backend &>> /dev/null
+systemctl start backend &>> $logFileLocation
 checkStatus "starting-backend"
 
 printHead "Install-mysql"
-dnf install mysql -y &>> /dev/null
+dnf install mysql -y &>> $logFileLocation
 checkStatus "Install-mysql"
 
 printHead "loading-schema"
-mysql -h 172.31.18.205 -uroot -pExpenseApp@1 < /app/schema/backend.sql &>> /dev/null
+mysql -h 172.31.18.205 -uroot -pExpenseApp@1 < /app/schema/backend.sql &>> $logFileLocation
 checkStatus "loading-schema"
 
