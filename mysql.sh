6b397088690d1be71b992572f3b83c8b18eb3d23
chaitu1 <chaitu143@gmail.com>
2024-03-05 14:45:22 +0530

    mysql changer

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,19 @@
-dnf install mysql-server -y
+function printHead () {
+ echo "------$1-----------"
+}
 
-systemctl enable mysqld
-systemctl start mysqld  
+printHead "Installing Mysql" 
+dnf install mysql-server -y &>> /dev/null
+echo $?
 
-mysql_secure_installation --set-root-pass ExpenseApp@1
+printHead "Enableing my sql"
+systemctl enable mysqld &>> /dev/null
+echo $?
+
+printHead "Starting Mysql"
+systemctl start mysqld   &>> /dev/null
+echo $?
+
+printHead "Setting Root Mysql password"
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>> /dev/null
+echo $?
