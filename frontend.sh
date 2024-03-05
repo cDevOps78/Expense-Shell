3b13f0e0b8483fc5bc15eb54b53e152595778b5f
chaitu1 <chaitu143@gmail.com>
2024-03-05 14:35:54 +0530

    This is new commit

--- frontend.sh
+++ frontend.sh
@@ -2,16 +2,18 @@ function printHead(){
  echo "$1"   
 }
 
+# printHead "Installing nginx"
+echo "Installing nginx"
 dnf install nginx -y &>> /tmp/frontend.log
 
 echo $?
-
+printHead "Starting nginx"
 systemctl start nginx  &>> /tmp/frontend.log
-
 echo $?
+printHead "Removing-content"
 rm -rf /usr/share/nginx/html/* 
 echo $?
-
+printHead "Dowlaod"
 curl --silent -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip 
 echo $?
 
