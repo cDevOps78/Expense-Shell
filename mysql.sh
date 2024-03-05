f5f3118099f76f8022980cdbae6e1771d2fb6be6
chaitu1 <chaitu143@gmail.com>
2024-03-05 21:24:38 +0530

    push

--- mysql.sh
+++ mysql.sh
@@ -16,7 +16,7 @@ checkStatus "Starting Mysq"
 
 
 printHead "Setting Root Mysql password"
-echo 'show databases' |  mysql -h 172.31.18.205 -uroot -pExpenseApp@1 &&>> $logFileLocation
+echo 'show databases' |  mysql -h 172.31.18.205 -uroot -pExpenseApp@1 &>> $logFileLocation
 if [ $? -ne 0 ];then
 mysql_secure_installation --set-root-pass ExpenseApp@1 &>> $logFileLocation
 fi
