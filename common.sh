609cb9eff95644b572eafd66924be2180f030ce4
chaitu1 <chaitu143@gmail.com>
2024-03-05 20:34:30 +0530

    remove T

--- common.sh
+++ common.sh
@@ -4,7 +4,7 @@ function checkStatus() {
 [ $? -ne '0' ] && echo -e "\n $R $1 is failure $N" && exit 1 ||  echo -e  "\n $G $1 is successfull $N" 
 
 }
-T
+
 function colours () {
     R="\e[31m"
     G="\e[32m"
