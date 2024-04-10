fafd902747f08a831eb1818dc1a61891985f34b3
chaitu1 <chaitu143@gmail.com>
2024-04-10 11:01:31 +0530

    commit and push

--- logstashFrontend.sh
+++ logstashFrontend.sh
@@ -2,7 +2,7 @@ R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 function statusCheck() {
-  [ $1 -ne 0 ] && echo -e "$2-${R}FAILED${N}" || echo -e "$2-${G}SUCCESS${N}"
+  [ $1 -ne 0 ] && echo -e "\n$2-${R}FAILED${N}" || echo -e "\n$2-${G}SUCCESS${N}"
 }
 
 echo '
@@ -18,7 +18,7 @@ type=rpm-md
 
 statusCheck $? "Set up elasticSearch repo"
 
-dnf install logstash
+dnf install logstash -y
 statusCheck $? "Installing logstash"
 
 #echo '
