a86a664f60e39e0243e070f8caa98ff85da5bb0f
chaitu1 <chaitu143@gmail.com>
2024-04-10 11:17:49 +0530

    commit and push

--- logstashFrontend.sh
+++ logstashFrontend.sh
@@ -1,9 +1,16 @@
+password=$1
+
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
+
+
 function statusCheck() {
-  [ $1 -ne 0 ] && echo -e "\n$2-${R}FAILED${N}" || echo -e "\n$2-${G}SUCCESS${N}"
+  echo "$2" &>> /tmp/logstash.log
+  [ $1 -ne 0 ] && echo -e "\n$2-${R}FAILED${N}" && echo "Check-Logs --> /tmp/logstash.log for more info!!" || echo -e "\n$2-${G}SUCCESS${N}"
 }
+
+> /tmp/logstash.log
 
 echo '
 [elasticsearch]
@@ -18,13 +25,25 @@ type=rpm-md
 
 statusCheck $? "Set up elasticSearch repo"
 
-dnf install logstash -y
+dnf install logstash -y  &>> /tmp/logstash.log
 statusCheck $? "Installing logstash"
 
-#echo '
-#input {
-#  file {
-#    path => "/var/log/nginx/access.log"
-#  }
-#}
-#' >
+echo "
+input {
+  file {
+    path => "/var/log/nginx/access.log"
+  }
+}
+
+output {
+    elasticsearch {
+        hosts => "https://172.31.66.169:9200"
+        data_stream => "true"
+        user => "elasticsearch"
+        ssl_verification_mode => "none"
+        password => "${password}"
+        index => "frontend-%{+yyyy.MM.dd}"
+    }
+}
+" > /etc/logstash/conf.d/frontend.conf
+statusCheck $? "Set up logstash conf file"
