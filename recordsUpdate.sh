1674b8213eceda096d53a5281f115d7cd42d42d1
chaitu1 <chaitu143@gmail.com>
2024-05-26 21:07:46 +0530

    working with config map

--- recordsUpdate.sh
+++ recordsUpdate.sh
@@ -1,3 +1,3 @@
 # To list hosted-zones
 
-aws route53  list-hosted-zones-by-name --dns-name azcart.online --query=HostedZones.Id
+aws route53  list-hosted-zones-by-name --dns-name azcart.online --query "HostedZones.Id"
