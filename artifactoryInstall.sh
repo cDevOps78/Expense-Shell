e1e1bae2ee4c43905321912800858102e1b3b5af
chaitu1 <chaitu143@gmail.com>
2024-05-27 17:33:40 +0530

    working on scheduling concept

---
+++ artifactoryInstall.sh
@@ -0,0 +1,30 @@
+curl -L https://releases.jfrog.io/artifactory/bintray-artifactory/org/artifactory/oss/jfrog-artifactory-oss/7.71.9/jfrog-artifactory-oss-7.71.9-linux.tar.gz -o /opt/jfrog-artifactory-oss-7.71.9-linux.tar.gz
+
+tar -xzf /opt/jfrog-artifactory-oss-7.71.9-linux.tar.gz -C /opt
+
+useradd artifactory
+
+chown -R artifactory:artifactory /opt/artifactory-oss-7.71.9
+
+echo '
+[Unit]
+Description=Artifactory service
+After=network.target
+
+[Service]
+SuccessExitStatus=143
+Type=forking
+GuessMainPID=yes
+Restart=always
+RestartSec=60
+PIDFile=/var/run/artifactory.pid
+ExecStart=/opt/artifactory-oss-7.71.9/app/bin/artifactoryManage.sh start
+ExecStop=/opt/artifactory-oss-7.71.9/app/bin/artifactoryManage.sh stop
+
+[Install]
+WantedBy=multi-user.target
+Alias=artifactory.service
+' > /etc/systemd/system/artifactory.service
+
+systemctl start artifactory
+systemctl status artifactory
