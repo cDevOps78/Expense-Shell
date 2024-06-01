f52c581bce2f33f6a7ec151a75ee61895684110c
chaitu1 <chaitu143@gmail.com>
2024-06-02 00:24:11 +0530

    create ec2 instance through shell script

---
+++ create-servers.sh
@@ -0,0 +1,19 @@
+
+ami_id="ami-0e4fd655fb4e26c30"
+security_group_id="sg-098c66671a7255c8e"
+subnet_id="subnet-05bc9c749607ae9c2"
+
+create_instance() {
+  aws ec2 run-instances \
+    --image-id ${ami_id} \
+    --instance-type t2.micro \
+    --subnet-id ${subnet_id} \
+    --security-group-ids ${security_group_id} \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}}]" \
+    --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"
+}
+
+for component in frontend
+do
+  create_instance
+done
